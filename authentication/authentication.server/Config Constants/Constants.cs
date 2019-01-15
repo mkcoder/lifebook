dc46acde821f73e3bb1700447cc2babf4c4bf59d
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-01-14 22:53:17 -0600

    inital build for admin ui
    inital configuration for admin ui credientials

---
+++ authentication/authentication.server/Config Constants/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace authentication.server.ConfigConstants
+{
+    public static class Constants
+    {
+        public struct ClaimTypes
+        {
+
+        }
+
+        public struct IdentityResourcesTypes
+        {
+
+        }
+
+        public struct Scopes
+        {
+            public const string OPEN_ID = "openid";
+            public const string EMAIL = "email";
+        }
+    }
+}
---
+++ authentication/authentication.server/InMemoryResources.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using authentication.server.ConfigConstants;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace authentication.server
+{
+    public static class InMemoryResources
+    {
+        public static IEnumerable<Client> GetInmemoryClients()
+            => new List<Client>()
+            {
+                new Client
+                {
+                    ClientId = "admin.ui",
+                    ClientName = "admin",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RedirectUris = {"" },
+                    AllowedScopes = { Constants.Scopes.OPEN_ID, Constants.Scopes.EMAIL },
+                }
+            };
+
+
+        public static IEnumerable<IdentityResource> GetInMemoryIdentityResources()
+            => new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> GetInMemoryApiResources()
+            => new List<ApiResource>
+            {
+                new ApiResource
+                {
+                     Name = "admin.ui",
+                     DisplayName = "internal application",
+                     UserClaims = {},
+                     Scopes = {}
+                }
+            };
+
+        public static List<TestUser> GetTestUsers()
+            => new List<TestUser>
+            {
+                new TestUser()
+                {
+                    Username ="tester",
+                    Password="password",
+                    SubjectId="tester"
+
+                }
+            };
+    }
+}
