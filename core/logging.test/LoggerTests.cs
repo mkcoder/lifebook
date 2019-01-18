16dc0c7f63ee7d9e69515a5685a37742e35660c8
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-01-17 21:28:56 -0600

    add logging
    added logger to core which write to elastic search and kibana

---
+++ core/logging.test/LoggerTests.cs
@@ -0,0 +1,19 @@
+using System;
+using lifebook.core.logging;
+using Xunit;
+
+namespace lifebook.core.logging.test
+{
+    public class UnitTest1
+    {
+        Logger _sut = new Logger();
+
+        [Fact]
+        public void Logging_Error_Logs_Error()
+        {
+            _sut.Error("Hello world");
+            _sut.Information("Hello world");
+            _sut.Verbose("Hello world");
+        }
+    }
+}
---
+++ core/logging/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace lifebook.core.logging
+{
+    public class Logger
+    {
+
+        protected Serilog.Core.Logger _logger;
+
+        public Logger()
+        {
+            _logger = new LoggerConfiguration()
+                            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                            {
+                                AutoRegisterTemplate = true,
+                                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6
+                            }).CreateLogger();
+        }
+
+        public void Error(string message) => _logger.Error(message);
+        public void Information(string message) => _logger.Information(message);
+        public void Verbose(string message) => _logger.Verbose(message);        
+    }
+}
