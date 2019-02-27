92bb783ecfba3bb372f1a9f6abe6e84d31da14cc
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-02-26 22:38:32 -0600

    #12 inital start of the react app for calendar
    start on the intial building blocks for the react app
    - todo create a custom babel js file to compile and don't take the easy way out .

---
+++ lifebook.app.calendar/lifebook.app.calendar/lifebook.app.webapp/src/components/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+
+export default class extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>My calendar</p>
+            </React.Fragment>
+        );
+    }
+}
---
+++ lifebook.app.calendar/lifebook.app.calendar/lifebook.app.webapp/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Calendar from './components/Calendar'
+
+render(<Calendar />, document.querySelector("#app"));
