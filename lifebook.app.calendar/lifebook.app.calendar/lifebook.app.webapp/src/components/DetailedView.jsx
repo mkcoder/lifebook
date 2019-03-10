a0ee1f35ae8f9640c9da8b37a807771ccf9a468d
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-03-09 23:43:26 -0600

    #12 need to do style
    refactor the app to use react need to style the application now

--- lifebook.app.calendar/lifebook.app.calendar/lifebook.app.webapp/src/components/DetailedView.jsx
+++ lifebook.app.calendar/lifebook.app.calendar/lifebook.app.webapp/src/components/DetailedView.jsx
@@ -1,18 +1,76 @@
 import React from 'react';
 
 export default class DetailedView extends React.Component {
-    RenderHeader() {
+    RenderHeader(selectedDates) {
         var header = [];
+        // timestamp column
+        header.push((
+            <th></th> 
+        ));
+        for (let start = selectedDates.start().getDate(); start <= selectedDates.end().getDate(); start++)
+        {
+            header.push((
+                <th>{start}</th>
+            ));
+        }
         return header;
     }
-    RenderDetailedView (date, intv, cols, selectedDates) {
+    GetEventCols(selectedDates, time, intv) {
+        // we will have to make an api call here
+        var header = [];
+        // timestamp column
+        for (let start = selectedDates.start().getDate(); start <= selectedDates.end().getDate(); start++)
+        {
+            header.push((
+                <td></td>
+            ));
+        }
+        return header;
+    }
+    RenderBody(selectedDates, intervals) {
+        debugger;
+        var tr = [];
+        var hoursDiv = 60/intervals;
+        for(let i = 0, time = 12, intv=0; i < 24*hoursDiv; i++) {
+            var ths = [];
+            ths.push((
+                <td>{time}:{intv < 9 ? "0"+intv : intv}</td>                
+            ));            
+            ths.push(this.GetEventCols(selectedDates, time, intv));
+            tr.push((
+                <tr>{ths}</tr>
+            ))
+            intv += intervals;
+            if(intv === 60) {
+                intv = 0;
+                if(time===12) time = 1;
+                else time++;
+            }
+        }
+        return tr;
+    }
+    RenderDetailedView (selectedDates, intv) {
         console.log(selectedDates.start());
-        return (<p>test</p>);
+        console.log(selectedDates.end());
+        return (
+            <React.Fragment>
+                <table>
+                    <thead>
+                        <tr>
+                            {this.RenderHeader(selectedDates)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.RenderBody(selectedDates, intv)}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
     }
     render() {
         return (
             <React.Fragment>
-                {this.RenderDetailedView(this.props.date, this.props.intervals, 7, this.props.selectedDates)}
+                {this.RenderDetailedView(this.props.selectedDates, this.props.intervals)}
             </React.Fragment>
         );
     }
