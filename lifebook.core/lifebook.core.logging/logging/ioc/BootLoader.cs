91d03c320b983d9ad7eb6fc49bb93e03cf0729b6
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-04-11 21:35:11 -0500

    updated all nuget packages to 1.0.10

--- lifebook.core/lifebook.core.logging/logging/ioc/BootLoader.cs
+++ lifebook.core/lifebook.core.logging/logging/ioc/BootLoader.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using lifebook.core.logging.interfaces;
+using lifebook.core.logging.services;
+
 namespace lifebook.core.logging.ioc
 {
-    public class BootLoader
+    public class BootLoader : IWindsorInstaller
     {
-        public BootLoader()
+        public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            container.Register(
+                Component.For<ILogger>().ImplementedBy<Logger>().LifestyleTransient()
+            );
         }
     }
 }
