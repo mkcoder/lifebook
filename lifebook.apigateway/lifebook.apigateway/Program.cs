10b24502cac9027626b715ccf79f6bcefcbea19a
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-04-16 22:25:12 -0500

    now i added the api gateway lol

---
+++ lifebook.apigateway/lifebook.apigateway/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+namespace lifebook.apigateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", true, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddJsonFile("ocelot.json")
+                       .AddEnvironmentVariables();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .UseIIS()
+               .Configure(app =>
+               {
+                   app.UseOcelot().Wait();
+               })
+               .UseStartup<Startup>()
+               .Build()
+               .Run();
+        }
+    }
+}
