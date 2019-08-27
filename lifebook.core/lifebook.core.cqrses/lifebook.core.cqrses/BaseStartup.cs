3ca09b577a5722d91fcd78a848398e274d9ed1cb
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-08-26 22:27:24 -0500

    inital work for cqrs + es project

---
+++ lifebook.core/lifebook.core.cqrses/lifebook.core.cqrses/BaseStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace lifebook.core.cqrses
+{
+    public class BaseStartup
+    { 
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+    }
+}
---
+++ lifebook.core/lifebook.core.cqrses/lifebook.core.cqrses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace lifebook.core.cqrses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<BaseStartup>().Build().Run();
+        }
+    }
+}
