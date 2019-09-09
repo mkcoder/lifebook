c753c3a20aef5211048b57a2a19c8e2e9f07dd3e
muhammad k khan <mk_muhammad.khan@hotmail.com>
2019-09-08 21:31:28 -0500

    Update CommandHandler.cs

--- lifebook.core/lifebook.core.cqrses/lifebook.core.cqrses/Filters/CommandHandler.cs
+++ lifebook.core/lifebook.core.cqrses/lifebook.core.cqrses/Filters/CommandHandler.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Windsor;
 using lifebook.core.cqrses.Attributes;
 using lifebook.core.cqrses.Domains;
 using lifebook.core.cqrses.Services;
 using lifebook.core.eventstore.domain.api;
 using lifebook.core.eventstore.domain.models;
+using lifebook.core.eventstore.extensions;
+using lifebook.core.eventstore.services;
+using lifebook.core.logging.interfaces;
+using lifebook.core.services.interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -16,20 +22,39 @@ namespace lifebook.core.cqrses.Filters
     {
         private readonly IEventReader _eventReader;
         private readonly IEventWriter _eventWriter;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+        private Guid _aggregateId = Guid.Empty;
+        private Guid _correlationId = Guid.Empty;
+        private Guid _causationId = Guid.Empty;
+        private string _aggregateType = null;
 
-        public CommandHandler(IEventReader eventReader, IEventWriter eventWriter)
+        public CommandHandler(IServiceProvider serviceProvider, IConfiguration configuration, ILogger logger)
         {
-            _eventReader = eventReader;
-            _eventWriter = eventWriter;
+            var container = (WindsorContainer)serviceProvider.GetService(typeof(IWindsorContainer));
+            if(!container.Kernel.HasComponent(typeof(IEventStoreClient)))
+            {
+                container.InstallEventStore();
+            }
+            _eventReader = container.Resolve<IEventReader>();
+            _eventWriter = container.Resolve<IEventWriter>();
+            _configuration = configuration;
+            _logger = logger;
         }
 
         public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             var isCommandHandler = ((ControllerActionDescriptor)context.ActionDescriptor).MethodInfo.GetCustomAttributes(typeof(CommandHandlerFor), true);
             if (isCommandHandler.Any())
-            {
-                Task.Run(() => Console.WriteLine(context));
-                ((ICommand)context.ActionArguments.Single().Value).IsValid();
+            {                
+                var command = (Command)context.ActionArguments.Single().Value;
+                _logger.Information($"Command read {command}");
+                command.IsValid();
+                command.CorrelationId = Guid.NewGuid();
+                _aggregateType = command.AggregateType;
+                _aggregateId = command.AggregateId;
+                _correlationId = command.CorrelationId;
+                _causationId = command.CommandId;
             }
             return next();
         }
@@ -40,9 +65,15 @@ namespace lifebook.core.cqrses.Filters
             if (isCommandHandler.Any())
             {   
                 var result = ((ObjectResult)context.Result).Value;
-                if (result is Event @e)
-                {
+                if (result is AggregateEvent @e)
+                {                    
+                    @e.CommandName = ((CommandHandlerFor)isCommandHandler[0]).Template;
+                    @e.EntityId = _aggregateId;
+                    @e.CorrelationId = _correlationId;
+                    @e.CausationId = _causationId;
                     ((ObjectResult)context.Result).Value = @e;
+                    _logger.Information($"Aggregate Event write {((ObjectResult)context.Result).Value}");
+                    _eventWriter.WriteEventAsync(new StreamCategorySpecifier(_configuration["ServiceName"], _configuration["ServiceInstance"], _aggregateType, e.EntityId), @e);
                 }
                 else
                 {
@@ -51,6 +82,11 @@ namespace lifebook.core.cqrses.Filters
 
             }            
             return next();
+        }
+
+        private string GetAggregateFromRequest(PathString path)
+        {
+            return path.Value.Split('/').First();
         }
 
         public Task OnExceptionAsync(ExceptionContext context)
