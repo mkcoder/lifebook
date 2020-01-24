4bc6bcad960630e4c2a82045519162940c146212
Muhammad Khan <mk_muhammad.khan@hotmail.com>
2020-01-24 16:43:22 -0600

    started work on the admin portal

---
+++ SchoolBook/SchoolBook.Admin/Domain/IFormRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Services;
+
+namespace WebApplication2.Domain
+{
+    public interface IFormRepository
+    {
+        List<Form> GetAll();
+        void Add(Form form);
+    }
+}
---
+++ SchoolBook/SchoolBook.Admin/Pages/Forms/Create.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Domain;
+using WebApplication2.Services;
+
+namespace WebApplication2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IFormRepository formRepository;
+        public CreateModel(IFormRepository formRepository)
+        {
+            this.formRepository = formRepository;
+        }
+
+        public PageResult OnGet()
+        {
+            return Page();
+        }
+
+        public PageResult OnPost(Form form)
+        {
+            formRepository.Add(form);
+            return Page();
+        }
+    }
+}
